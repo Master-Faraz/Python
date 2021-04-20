c0b54d78166dfefb9e4d0b3dd7e96f880b21460d
Faraz Ali Ahmad <farazali9028@gmail.com>
2021-04-20 05:50:34 +0530

    codechef

---
+++ Basic/chocolate_Monger_CodeChef.py
@@ -0,0 +1,24 @@
+t = int(input())
+ls = []
+
+while(t):
+    res = 0
+    l = 0
+    n, x = input().split(" ")
+    n = int(n)
+    x = int(x)
+
+    for i in range(n):
+        a = int(input())
+        if a not in ls:
+            ls.append(a)
+
+    res = n-x
+    l = len(ls)
+
+    if res <= l:
+        print(res)
+    else:
+        print(l)
+
+    t = t-1
