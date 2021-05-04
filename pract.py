be23f3609abc570daa8ac8ffb299834f1085998f
Faraz Ali Ahmad <farazali9028@gmail.com>
2021-05-04 21:20:21 +0530

    updated

---
+++ pract.py
@@ -0,0 +1,13 @@
+n=int(input("Enter size of list you want to create : "))
+ls=[]
+
+for i in range(n):
+    a=int(input(f"Enter {i} th Number : "))
+    ls.append(a)
+
+s=set(ls)
+ls=list(s)
+ls.sort(reverse=True)
+print(ls)
+
+
