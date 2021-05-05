454139d959849a7fa34ab6d19a33828de87770ce
Faraz Ali Ahmad <farazali9028@gmail.com>
2021-05-05 12:53:41 +0530

    numpy

---
+++ Data Science and Machine Learning/Numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], np.int64)
+
+
+# print(arr[0])
+# print(arr.dtype)
+
+"""
+np.int64 is dtype of our array  -->  By Default it is int32
+np.int64 is optional
+
+"""
+arr_2D = np.array([[1, 2, 3], [3, 4, 5], [5, 6, 7]])  # 2D Array
+
+print(arr_2D.shape)     #    Shape will return Dimension of array
+print(arr_2D.dtype)
+print(arr_2D.dtype)
