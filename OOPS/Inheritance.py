332d65c494cc3bc3e0b584e3e3c4448ba442b6c0
Master-Faraz <farazali9028@gmail.com>
2021-05-11 05:42:40 +0530

    done

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -1,40 +1,40 @@
-# class Parent(object):  # .                    Every class inherited from object class
-#     a = 50
-#     __b = 60  # .                      Prtected Method
+class Parent(object):  # .                    Every class inherited from object class
+    a = 50
+    __b = 60  # .                      Prtected Method
 
-#     @classmethod  # .                          Without this our function will not work
-#     def f_Fun(self):
-#         print(f"Parent function data type {self.a}")
+    @classmethod  # .                          Without this our function will not work
+    def f_Fun(self):
+        print(f"Parent function data type {self.a}")
 
-#     def rint():
-#         print("Normal function in parent class")
-
-
-# class Child(Parent):  # .                      Child inherited from Parent class
-#     c = 90
-
-#     def __init__(self):
-#         super().__init__()
-
-#     @classmethod  # .                          Without this our function will not work
-#     def P_Fun(self):
-#         print(f"child function data type {self.c}")
-
-#     def rint(self):
-#         print("Normal function in child class")
+    def rint():
+        print("Normal function in parent class")
 
 
-# obj = Child()
+class Child(Parent):  # .                      Child inherited from Parent class
+    c = 90
 
-# print(obj.c)
-# print(obj.a)
+    def __init__(self):
+        super().__init__()
 
-# obj.P_Fun()
-# obj.rint()
+    @classmethod  # .                          Without this our function will not work
+    def P_Fun(self):
+        print(f"child function data type {self.c}")
+
+    def rint(self):
+        print("Normal function in child class")
+
+
+obj = Child()
+
+print(obj.c)
+print(obj.a)
+
+obj.P_Fun()
+obj.rint()
 
 # .                      It returns true if obj is instance of child class
-# x = isinstance(obj, Child)
-# print(x)
+x = isinstance(obj, Child)
+print(x)
 
 
 """                            *****        Method Overriding       *****
@@ -51,10 +51,19 @@ class Parent:
 class Child(Parent):
 
     def __init__(self):
+        super().__init__()  # .          For not getting erreo
         print("Child constructor ")
         self.weight = 50
 
-b=Child()
+# class Grand_Child( Child ): #.                  Multilevel Inheritance
+#     pass
 
-print(b.age())      #.                  This will give an error as w
-print(b.weight())
+# class Multiple( Parent , Child ):   #.              Multilevel inheritance
+#     pass
+
+
+b = Child()
+
+# .         This will give an error as child constructor will override the parent constructor
+print(b.age)
+print(b.weight)
