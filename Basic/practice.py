8238cfc3f29e1552d0dd731f722fb03aa661c4f5
Master-Faraz <farazali9028@gmail.com>
2021-05-14 21:08:50 +0530

    done

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,24 +1,23 @@
-t = int(input())
-ls = []
+from typing import NewType
 
-while(t):
-    res = 0
-    l = 0
-    n, x = input().split(" ")
-    n = int(n)
-    x = int(x)
 
-    for i in range(n):
-        a = int(input())
-        if a not in ls:
-            ls.append(a)
+l = []
+for i in range(0, 3):
+    tic = input()
+    l.append(tic)
 
-    res = n-x
-    l = len(ls)
-
-    if res <= l:
-        print(res)
-    else:
-        print(l)
-
-    t = t-1
+cx = 0
+co = 0
+n = 0
+# print(l)
+for i in l:
+    for j in i:
+        if(j == 'X'):
+            cx += 1
+        elif(j == 'O'):
+            co += 1
+        elif(j == '_'):
+            n += 1
+print(cx)
+print(co)
+print(n)
