36fc0a4d3ebf3eaf4e4e062f127b66dfc028237c
Master-Faraz <farazali9028@gmail.com>
2021-06-23 14:29:37 +0530

    Gradient descent code

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,23 +1,8 @@
-from typing import NewType
+n=5
+for i in range(10):
+    if i<n:
+        n=i
+    elif i>n:
+        break
 
-
-l = []
-for i in range(0, 3):
-    tic = input()
-    l.append(tic)
-
-cx = 0
-co = 0
-n = 0
-# print(l)
-for i in l:
-    for j in i:
-        if(j == 'X'):
-            cx += 1
-        elif(j == 'O'):
-            co += 1
-        elif(j == '_'):
-            n += 1
-print(cx)
-print(co)
-print(n)
+    print(i)
