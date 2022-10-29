64be31df1bd18075ffff31dddd54ce75a47b5ab7
Predator <farazali9028@gmail.com>
2022-10-29 17:46:54 +0530

    Updated

--- pract.py
+++ pract.py
@@ -1,1 +1,15 @@
-print("Hello world")
+def bubble_sort(arr):
+    array_length = len(arr)
+    for i in range(array_length):
+        for j in range(array_length):
+            if (arr[i] < arr[j]):
+                arr[i], arr[j] = arr[j], arr[i]
+
+
+
+
+
+
+my_array = [32, 56, 11, 45, 66, 1, 3, 77, 25]
+bubble_sort(my_array)
+print(my_array)
